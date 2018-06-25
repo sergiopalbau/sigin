a1092e737ff5489b8f070aedf6e67aaee66c1544
sergiopalbau <sergio.palbau@educa.jcyl.es>
2018-06-25 18:56:53 +0200

    Initial commit

---
+++ www/js/logica.js
@@ -0,0 +1,46 @@
+var app={
+
+	inicio: function(){
+
+		var nuevo= document.querySelector('#nuevo');
+		var registro = document.querySelector('#registro');
+		var subir = document.querySelector('#subir');
+		var rst = document.querySelector('#rst');
+
+		//nuevo.addEventListener('click', this.nuevo,false);
+		//registro.addEventListener('click',this.registro, false);
+		//subir.addEventListener('click', this.subir, false);
+		//rst.addEventListener('click', this.rst, false);
+
+	},
+
+	nuevo: function (){
+		
+		
+
+	},
+
+	registro: function (){
+		
+
+	},
+
+	subir: function (){
+		
+
+
+	},
+
+	rst: function (){
+		
+	}
+
+
+	
+}
+window.addEventListener('load',inicio2,false);
+
+function inicio2 (){
+	app.inicio();
+
+}
